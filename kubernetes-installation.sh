819d609a06740fcddc9fed5c09f33ecadc4ecef2
DimSalPro <dsalmatanis@gmail.com>
2023-04-25 11:58:17 +0300

    fixed IP list

--- kubernetes-installation.sh
+++ kubernetes-installation.sh
@@ -8,7 +8,8 @@ echo ""
 chmod +x "$(pwd)/ConfigurationFiles/configuration_server_script.sh"
 
 # Step 2: Extract the list of IP addresses from the IP_ADDRESS file, ignoring any comments
-ip_addresses="$(grep -vE '^#|^\s*$' "$(pwd)/ConfigurationFiles/IP_ADDRESS")"
+# ip_addresses="$(grep -vE '^#|^\s*$' "$(pwd)/ConfigurationFiles/IP_ADDRESS")"
+ip_addresses="$(grep -vE '^#|^\s*$' "$(pwd)/ConfigurationFiles/IP_ADDRESS" | sed 's/#.*//')"
 echo "$ip_addresses"
 # Extract the IP address of the master node from the IP_ADDRESS file
 master_ip=$(grep -E '^[^#]*\smaster\s*#' "$(pwd)/ConfigurationFiles/IP_ADDRESS" | awk '{print $1}')
