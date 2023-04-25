e4ba85dc845947bc8e46fd13cf46a5d1d01cea13
DimSalPro <dsalmatanis@gmail.com>
2023-04-25 12:07:31 +0300

    fixed ssh pass auth

--- kubernetes-installation.sh
+++ kubernetes-installation.sh
@@ -39,7 +39,7 @@ do
             # Make the copied file executable
             sshpass -p $password ssh -o StrictHostKeyChecking=no "$hostname@$ip" "chmod +x ~/node_script_before_rke.sh"
             # Run the copied file with sudo
-            sshpass -p $password ssh -o StrictHostKeyChecking=no "$hostname@$ip" "echo $password | sudo -S ~/node_script_before_rke.sh"
+            sshpass -p $password ssh -o StrictHostKeyChecking=no "$hostname@$ip" "sudo -S ~/node_script_before_rke.sh"
             # Check if the file was executed successfully
             if [ $? -eq 0 ]; then
                 echo "Script executed successfully on $ip"
